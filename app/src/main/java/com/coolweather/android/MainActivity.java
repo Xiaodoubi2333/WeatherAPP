3310f67729c5891d1a8f986c4d9346363fd9ef82
Xiaodoubi2333 <crazysnail2133@gmail.com>
2024-06-09 09:39:17 +0800

    第二次提交：androidx升级成功，测试原功能正常.

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -3,8 +3,8 @@ package com.coolweather.android;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
